c063be11ec8bc58fe1dbe480c8f6e2242e39ee62
Brittany Hemming <brittany.hemming@gmail.com>
2017-10-22 20:53:39 +0700

    changes

---
+++ JavaScript/scripts.js
@@ -0,0 +1,56 @@
+$( document ).ready(function() {
+// JQuery Goes Here
+// Smooth Scroll
+function scrollToSection(event) {
+
+  // Log
+  console.log('Clicked on anchor element');
+
+  // Prevent jumping to the section (default behaviour)
+  event.preventDefault();
+
+  // Get anchor element hash
+  var element = event.currentTarget,
+      hash = element.hash;
+
+  // Scroll the body and HTML to the ID equal to hash
+  $('html, body').animate({
+
+    // Find the offset of the targeted element on the page
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Set URL to clicked hash
+    window.location.hash = hash;
+
+  });
+
+}
+
+/*
+**  Bind events
+*/
+$('a').on('click', scrollToSection);
+
+// mobile menu
+function changeBodyClass(event) {
+
+  // Log
+  console.log('Clicked on toggle menu class');
+
+  // Prevent default event (clicking a link)
+  event.preventDefault();
+
+  // Toggle body class
+  $('#container').toggleClass('mobile-menu-visible');
+
+}
+
+/*
+**  Bind events
+*/
+$('a').on('click', changeBodyClass);
+
+
+//This closes JQuery
+});
